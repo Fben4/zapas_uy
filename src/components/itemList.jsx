0eb26d381032054f81c853f102001d46bc759ea5
Franco Benitez <fbenitezyferrer@gmail.com>
2022-09-09 03:10:55 -0300

    structure database

--- src/components/itemList.jsx
+++ src/components/itemList.jsx
@@ -3,52 +3,10 @@ import { Link } from "react-router-dom";
 import Item from './item';
 import { useEffect, useState } from 'react';
 import customFetch from '../utils/customFetch';
+import zapasfromBD from "../utils/data";
 
 
-const zapasfromBD = [
-    {
-        id: 1,
-        name:'Air Max 270',
-        thumbnail:require('../images/270.png') ,
-        cost:5500 ,
-        stock:4
-    },
-    {
-        id: 2,
-        name:'Air Max 90',
-        thumbnail:require('../images/airmax.png') ,
-        cost:8000 ,
-        stock:3
-    },
-    {
-        id: 3,
-        name:'Converse Weapon Hombre',
-        thumbnail:require('../images/weapon.png') ,
-        cost:9000 ,
-        stock:6
-    },
-    {
-        id: 4,
-        name:'Adidas x Bad Bunny',
-        thumbnail:require('../images/badbunny.png') ,
-        cost:8000 ,
-        stock:8
-    },
-    {
-        id: 5,
-        name:'Vans SK9 Hi',
-        thumbnail:require('../images/vanspro.png') ,
-        cost:6000 ,
-        stock:7
-    },
-    {
-        id: 6,
-        name:'Puma Wild Rider',
-        thumbnail:require('../images/wildrider.png') ,
-        cost:7000 ,
-        stock:4
-    }
-]
+
 
 const ItemList = () => {
 
